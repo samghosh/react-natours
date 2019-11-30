9432730f81e13da6794bf51f6feb7f37c0f344f3
Samrat Ghosh <samghosh@deloitte.com>
2019-12-01 00:41:36 +0530

    Project Updated to suit a different portfolio

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,9 @@
 import React from 'react'; 
-import Header from '../Components/Header/Header';
 
 const Home = () => {
-    const headerButtonText = 'discover our tours';
-    const headerPrimaryText = 'Outdoors';
-    const headerSubText = 'is where life happens';
-
     return (
         <>
-            <Header 
-                buttonText={headerButtonText}
-                HeaderPrimaryText={headerPrimaryText}
-                HeaderSubText={headerSubText}
-            />
+            <h1>Hello World</h1> 
         </>
     )
 };
